e62538ebd852865ed3aec6c751e02a3be5ef5fad
garrickcheng <garrickcheng2022@gmail.com>
2025-10-17 16:25:40 -0400

    a3 JavaScript

--- app/(Kambaz)/Account/Navigation.tsx
+++ app/(Kambaz)/Account/Navigation.tsx
@@ -1,10 +1,45 @@
+"use client";
 import Link from "next/link";
+import { usePathname } from "next/navigation";
+
 export default function AccountNavigation() {
+    const pathname = usePathname();
+    
+    const links = [
+        { label: "Signin", path: "/Account/Signin" },
+        { label: "Signup", path: "/Account/Signup" },
+        { label: "Profile", path: "/Account/Profile" }
+    ];
+
     return (
-        <div id="wd-account-navigation">
-            <Link href="Signin"> Signin </Link> <br />
-            <Link href="Signup"> Signup </Link> <br />
-            <Link href="Profile"> Profile </Link> <br />
-        </div>
+        <>
+            <style dangerouslySetInnerHTML={{
+                __html: `
+                    .list-group .account-nav-link:hover {
+                        text-decoration: underline !important;
+                        border-bottom: 1px solid currentColor;
+                    }
+                `
+            }} />
+            <div id="wd-account-navigation" className="list-group fs-5 rounded-0" style={{ minWidth: "200px" }}>
+                {links.map((link) => {
+                    const isActive = pathname.includes(link.label);
+                    return (
+                        <Link 
+                            key={link.label}
+                            href={link.path}
+                            className={`list-group-item border-0 account-nav-link text-decoration-none ${
+                                isActive ? "text-dark" : "text-danger"
+                            }`}
+                            style={{ 
+                                backgroundColor: "transparent"
+                            }}
+                        >
+                            {link.label}
+                        </Link>
+                    );
+                })}
+            </div>
+        </>
     );
 }
