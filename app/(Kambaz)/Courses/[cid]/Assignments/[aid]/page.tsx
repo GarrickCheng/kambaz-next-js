8694bf47115e4e3d731d0ce6dbf1f7e70fb941e3
garrickcheng <garrickcheng2022@gmail.com>
2025-12-02 01:07:31 -0500

    fix

--- app/(Kambaz)/Courses/[cid]/Assignments/[aid]/page.tsx
+++ app/(Kambaz)/Courses/[cid]/Assignments/[aid]/page.tsx
@@ -43,11 +43,25 @@ export default function AssignmentEditor() {
     const handleSave = async () => {
       if (aid === "Editor") {
         // Creating new assignment
-        const newAssignment = await coursesClient.createAssignment(cid as string, assignment);
+        const assignmentData = {
+          title: assignment.title,
+          description: assignment.description,
+          points: assignment.points,
+          dueDate: assignment.dueDate,
+          availableDate: assignment.availableDate,
+        };
+        const newAssignment = await coursesClient.createAssignment(cid as string, assignmentData);
         dispatch(addAssignment(newAssignment));
       } else {
         // Updating existing assignment
-        await coursesClient.updateAssignment(aid as string, assignment);
+        const assignmentData = {
+          title: assignment.title,
+          description: assignment.description,
+          points: assignment.points,
+          dueDate: assignment.dueDate,
+          availableDate: assignment.availableDate,
+        };
+        await coursesClient.updateAssignment(aid as string, assignmentData);
         dispatch(updateAssignment(assignment));
       }
       router.push(`/Courses/${cid}/Assignments`);
